f090d33291ef680124bc79f9f6224c848ba9bd6e
tungtvt <trinhvutung87@gmail.com>
2020-09-17 17:23:33 +0700

    Fix bug: prevent send refreshToken request when get new token fail

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
-import { BE_URL, LOGIN_URL } from "./constant";
 import authService from "../services/auth.service";
+import { BE_URL, LOGIN_URL, REFRESH_TOKEN_URL } from "./constant";
+
 
 const api_instance = axios.create({
   baseURL: BE_URL
@@ -15,7 +15,7 @@ api_instance.interceptors.request.use(
 
     if (token && !isExpire) {
       config.headers.authorization = `Bearer ${token}`;
-      if (isRefresh) {
+      if (isRefresh && config.url !== REFRESH_TOKEN_URL) {
         authService.getRefreshToken();
       }
     } else if (config.url !== LOGIN_URL) {
