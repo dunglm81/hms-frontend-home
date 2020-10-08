f34a29d16df76d6a55c3eef6f6a06c9bcfbca532
tungtvt <trinhvutung87@gmail.com>
2020-10-08 14:16:59 +0700

    Hide version-name element

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
             </Switch>
           </div>
         </Router>
-        <div className="version-name">Phiên bản UAT</div>
+        {/* <div className="version-name">Phiên bản UAT</div> */}
       </div>
     );
   }
