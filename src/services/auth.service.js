4b24b73abf800b99b577bfb28eb84c0e8380efb1
tungtvt <trinhvutung87@gmail.com>
2020-10-08 16:40:06 +0700

    Fix bug: Cannot refresh token

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -133,7 +133,7 @@ class AuthService {
       .get(REFRESH_TOKEN_URL)
       .then((response) => {
         if (response.status === 200) {
-          this.setAccessToken(response.data.token);
+          this.setAccessToken(response.data.accessToken);
         }
       })
       .catch((err) => {
